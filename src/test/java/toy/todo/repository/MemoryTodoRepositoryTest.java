3a7104c7fc27969de8ed0df8c33ac6b4a2417580
neSTORY <nes2376@gmail.com>
2025-05-06 14:20:12 +0900

    test: test update, delete feature of repository

--- src/test/java/toy/todo/repository/MemoryTodoRepositoryTest.java
+++ src/test/java/toy/todo/repository/MemoryTodoRepositoryTest.java
@@ -3,6 +3,7 @@ package toy.todo.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import toy.todo.domain.Todo;
@@ -45,11 +46,49 @@ class MemoryTodoRepositoryTest {
     @Test
     public void findAll() throws Exception {
         //given
+        Todo todo1 = new Todo("정보처리기사", "1회 기출풀기");
+        Todo todo2 = new Todo("스프링", "MVC2 1강 듣기");
+
+        repository.save(todo1);
+        repository.save(todo2);
 
         //when
+        List<Todo> allTodos = repository.findAll();
 
         //then
+        assertThat(allTodos.size()).isEqualTo(2);
+    }
 
+    @Test
+    public void update() throws Exception {
+        //given
+        Todo todo1 = new Todo("정보처리기사", "1회 기출풀기");
+        repository.save(todo1);
+
+        //when
+        Todo updateParam = new Todo("sqld", "2회 기출 풀기");
+        repository.update(todo1.getId(), updateParam);
+
+        //then
+        Todo updatedTodo = repository.findById(todo1.getId());
+        assertThat(updatedTodo.getTitle()).isEqualTo("sqld");
+    }
+
+    @Test
+    public void delete() throws Exception {
+        //given
+        Todo todo1 = new Todo("정보처리기사", "1회 기출풀기");
+        Todo todo2 = new Todo("스프링", "MVC2 1강 듣기");
+
+        repository.save(todo1);
+        repository.save(todo2);
+
+        //when
+        repository.delete(todo1.getId());
+
+        //then
+        List<Todo> allTodo = repository.findAll();
+        assertThat(allTodo.size()).isEqualTo(1);
     }
 
 }
